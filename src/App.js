22f4856c7392ade7c22f511dc67d843b69d95fd5
raSel <mdraselm413@gmail.com>
2022-10-30 23:00:08 +0600

    load data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home></Home>
+      element: <Home></Home>,
+      loader: () => fetch('http://localhost:5000/users')
     },
     {
       path: '/users/add',
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
+
+    const handleDelete = user =>{
+        const aggree = window.confirm(`Are you sure you want to delet this user? ${user.name}`);
+        // console.log(aggree);
+            if(aggree){
+                console.log('Deleting User With ID:', user._id);
+            }
+    }
     return (
         <div>
-            <h2>This is Users Home: </h2>
+            <h2>This is Users Home:  </h2>
+            <h3>All Users: {users.length}</h3>
+            <div>
+                {
+                    users.map(user => <p key={user._id}>
+                        {user.name} 
+                        {user.email} 
+                        <button 
+                        onClick={() => handleDelete(user)}>
+                            X
+                        </button>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
