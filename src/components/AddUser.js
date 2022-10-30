0410803f17bd7b5ce98811a91bf93d29b25ba2e2
raSel <mdraselm413@gmail.com>
2022-10-30 22:10:25 +0600

    server pass data

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -8,6 +8,23 @@ const AddUser = () => {
         event.preventDefault();
         console.log(user);
 
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if (data.acknowledged) {
+                alert('User Added Successfully');
+                event.target.reset()
+            }
+        })
+
+
     }
 
     const handleInputBlur = event =>{
